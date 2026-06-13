22cd16676e2f08ecff412f21037689a542f4c554
Chakradebest <chakradharreddyvintha@gmail.com>
2026-06-13 19:13:40 -0400

    Configure next.config.ts and actions workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",      // <-- Instructs Next.js to generate static HTML
+  images: {
+    unoptimized: true,  // <-- Prevents image processing from throwing build errors
+  },
 };
 
 export default nextConfig;
